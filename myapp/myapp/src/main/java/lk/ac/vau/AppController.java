b8599a7cd8ef23ed8689df26122afdf33b4537b6
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2025-03-22 22:21:06 +0530

    Msgs initiated

--- myapp/myapp/src/main/java/lk/ac/vau/AppController.java
+++ myapp/myapp/src/main/java/lk/ac/vau/AppController.java
@@ -1,5 +1,22 @@
 package lk.ac.vau;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/app")
 public class AppController {
+    @GetMapping("/msg")
+    public String myMessage() {
+        return "Hello SpringBoot";
+    }
+    @GetMapping("/name")
+    public String myName() {
+        return "My name is SpringBoot";
+    }
+    
     
 }
